6e371802b8f90ec50587de10d1ad0383730a196d
Swapnayandatta <138855944+Swapnayandatta@users.noreply.github.com>
2025-03-13 00:29:14 +0530

    committed

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # PRODIGY_CS_03
- 
+🔐 Password Strength Checker
+📌 About
+This is a simple password strength checker written in Python. It evaluates a user's password based on security best practices and provides feedback on its strength.
+
+🚀 Features
+✔️ Checks if the password has at least 8 characters
+✔️ Ensures the password contains a digit (0-9)
+✔️ Verifies the presence of an uppercase letter (A-Z)
+✔️ Confirms the use of a lowercase letter (a-z)
+✔️ Requires at least one special character (!@#$%^&*(){}<>.?)
+✔️ Provides real-time feedback on password strength
+
+📜 How It Works
+1️⃣ The script continuously prompts the user to enter a password.
+2️⃣ If the user types "exit", the program stops.
+3️⃣ The script evaluates the password and returns one of the following messages:
+
+Weak Password: If it lacks minimum length, digits, uppercase, or lowercase letters.
+Medium Password: If it lacks special characters.
+Strong Password: If all criteria are met.
---
+++ pass_complexity_checker.py
@@ -0,0 +1,32 @@
+import re
+
+def strenght_checker(password):
+    if len(password) < 8 :
+        return "weak password : Password must be at least 8 character."
+    
+    if not any(char.isdigit() for char in password):
+        return "weak password : Password must containt a digit."
+    
+    if not any(char.isupper() for char in password):
+        return "weak password : Password must containt a Upper case."
+    
+    if not any(char.islower() for char in password):
+        return "weak password : Password must containt a Lower case."
+    
+    if not re.search(r'[!@#$%^&*(){}<>.?]', password):
+        return "Medium password : your password need at least one special character."
+    
+    return "Your password is strong and secure. "
+
+
+def pass_check():
+    print("Check your password strength with this tool now!")
+    while True:
+        password = input("Enter your password (if you want to quit then press exit): ")
+        if password.lower() == 'exit':
+            print("Thanks for using this tool")
+            break
+        result = strenght_checker(password)
+        print(result)
+
+pass_check()
